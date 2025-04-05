159a2c087029847da0cf26e567f88bb004fcc972
Mohammed Nazmy <mohammed.nazmy2@gmail.com>
2025-04-05 18:54:41 +0200

    Deploy Flutter web build to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3428276048"
+    serviceWorkerVersion: "467088376"
   }
 });
